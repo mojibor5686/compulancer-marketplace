b9e3a8eb2e48aeb7a95eb7e5b62f9e8dc92b0d0a
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-15 13:35:06 -0500

    Testing Push Data

--- core/routes/web.php
+++ core/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/clear', function () {
-    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    \Illuminate\Support\Facades\Artisan::call('optimize:clears');
 });
 
 Route::post('pusher/auth', 'SiteController@pusher')->name('pusher.auth');
