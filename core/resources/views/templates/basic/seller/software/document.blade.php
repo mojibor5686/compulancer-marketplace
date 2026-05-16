0d6444e7255acc4a03e5702bee8105ae8d5f7c11
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-16 10:15:25 -0500

    Update software document.blade.php: move labels above file inputs for consistent vertical layout

--- core/resources/views/templates/basic/seller/software/document.blade.php
+++ core/resources/views/templates/basic/seller/software/document.blade.php
@@ -4,58 +4,42 @@
         <form id="documentForm">
             <div class="gig-overview-space">
                 <!-- Documentation File Upload Section -->
-                <div class="row mb-4">
-                    <div class="col-lg-3">
-                        <div class="gig-overview__title">
-                            <h6>@lang('Documentation File')</h6>
+                <div class="form--group-lg">
+                    <label class="form-label form--label">@lang('Documentation File')</label>
+                    <div class="custom-file-wrapper">
+                        <div class="custom-file">
+                            <input class="custom-file-input" id="documentFile" name="document_file" type="file"
+                                accept=".pdf" required>
+                            <label class="custom-file-label" for="documentFile">@lang('Choose file')</label>
                         </div>
-                    </div>
-                    <div class="col-lg-9">
-                        <div class="gig-overview__form">
-                            <div class="custom-file-wrapper">
-                                <div class="custom-file">
-                                    <input class="custom-file-input" id="documentFile" name="document_file" type="file"
-                                        accept=".pdf" required>
-                                    <label class="custom-file-label" for="documentFile">@lang('Choose file')</label>
-                                </div>
-                                <p class="mt-1">@lang('Only .pdf file is supported. Document file is essential for user understanding.')</p>
-                                @if (@$software->document_file)
-                                    <small>@lang('Existing document'):</small>
-                                    <a href="{{ route('file.download', [encrypt($software->document_file), 'documentation']) }}"
-                                        title="@lang('Download Document')" data-bs-toggle="tooltip" data-bs-placement="top">
-                                        <i class="las la-download"></i>
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                        <p class="mt-1">@lang('Only .pdf file is supported. Document file is essential for user understanding.')</p>
+                        @if (@$software->document_file)
+                            <small>@lang('Existing document'):</small>
+                            <a href="{{ route('file.download', [encrypt($software->document_file), 'documentation']) }}"
+                                title="@lang('Download Document')" data-bs-toggle="tooltip" data-bs-placement="top">
+                                <i class="las la-download"></i>
+                            </a>
+                        @endif
                     </div>
                 </div>
 
                 <!-- Software File Upload Section -->
-                <div class="row mb-4">
-                    <div class="col-lg-3">
-                        <div class="gig-overview__title">
-                            <h6>@lang('Software File')</h6>
+                <div class="form--group-lg">
+                    <label class="form-label form--label">@lang('Software File')</label>
+                    <div class="custom-file-wrapper">
+                        <div class="custom-file">
+                            <input class="custom-file-input" id="softwareFile" name="software_file" type="file"
+                                accept=".zip" required>
+                            <label class="custom-file-label" for="softwareFile">@lang('Choose file')</label>
                         </div>
-                    </div>
-                    <div class="col-lg-9">
-                        <div class="gig-overview__form">
-                            <div class="custom-file-wrapper">
-                                <div class="custom-file">
-                                    <input class="custom-file-input" id="softwareFile" name="software_file" type="file"
-                                        accept=".zip" required>
-                                    <label class="custom-file-label" for="softwareFile">@lang('Choose file')</label>
-                                </div>
-                                <p class="mt-1">@lang('Only .zip file is supported.')</p>
-                                @if (@$software->software_file)
-                                    <small>@lang('Existing software'):</small>
-                                    <a href="{{ route('file.download', [encrypt($software->software_file), 'file']) }}"
-                                        title="@lang('Download Software')" data-bs-toggle="tooltip" data-bs-placement="top">
-                                        <i class="las la-download"></i>
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                        <p class="mt-1">@lang('Only .zip file is supported.')</p>
+                        @if (@$software->software_file)
+                            <small>@lang('Existing software'):</small>
+                            <a href="{{ route('file.download', [encrypt($software->software_file), 'file']) }}"
+                                title="@lang('Download Software')" data-bs-toggle="tooltip" data-bs-placement="top">
+                                <i class="las la-download"></i>
+                            </a>
+                        @endif
                     </div>
                 </div>
 
