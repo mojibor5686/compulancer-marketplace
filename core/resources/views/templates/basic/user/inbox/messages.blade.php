1e83894c44c5b5576a8f2cb770f30fd54f662f53
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-17 00:52:05 -0500

    color update

--- core/resources/views/templates/basic/user/inbox/messages.blade.php
+++ core/resources/views/templates/basic/user/inbox/messages.blade.php
@@ -111,7 +111,7 @@
                                                 accept=".jpg, .png, .jpeg, .pdf">
                                         </div>
                                         <button type="submit" class="btn btn--base send-btn"
-                                            style="display: inline-flex; font-size: 16px; align-items: center; justify-content: center; padding: 0.75rem 2.5rem; min-width: 8rem; border-radius: 5rem; background-color: #007bff; color: #ffffff; border: none; font-weight: 500;">
+                                            style="display: inline-flex; font-size: 16px; align-items: center; justify-content: center; padding: 0.75rem 2.5rem; min-width: 8rem; border-radius: 5rem; background-color: #3a84ff; color: #ffffff; border: none; font-weight: 500;">
                                             @lang('Send')
                                         </button>
                                     </div>
