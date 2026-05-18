7fd6aeaa88e30acf391ea3c9a32f4d21b9628e47
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-18 05:43:34 -0500

    updated

--- core/resources/views/templates/basic/partials/header.blade.php
+++ core/resources/views/templates/basic/partials/header.blade.php
@@ -164,9 +164,8 @@
                 </a>
 
                 @if (auth()->check())
-                    <a href="{{ route('user.profile') }}"
-                        class="d-block d-lg-none text-secondary font-weight-bold text-decoration-none small">My
-                        Profile</a>
+                    <a href=""
+                        class="d-block d-lg-none text-secondary font-weight-bold text-decoration-none small"></a>
                 @else
                     <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#signInModal"
                         class="d-block d-lg-none text-secondary font-weight-bold text-decoration-none small">Sign In</a>
