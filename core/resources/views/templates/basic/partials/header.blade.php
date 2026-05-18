e5f6f51056cedd9e1eec179caf1812c5ee7e1479
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-18 00:16:34 -0500

    update

--- core/resources/views/templates/basic/partials/header.blade.php
+++ core/resources/views/templates/basic/partials/header.blade.php
@@ -420,20 +420,13 @@
 
                 <div class="modal-body px-4 py-3">
 
-                    @if ($socialLoginActive)
-                        <div class="d-flex justify-content-center mb-2">
-                            @include('Template::partials.social_login')
-                        </div>
-
-                        <div class="position-relative text-center my-4">
-                            <hr class="text-muted opacity-25">
-                            <span
-                                class="position-absolute top-50 start-50 translate-middle bg-white px-3 text-muted small"
-                                style="font-size: 13px;">
-                                @lang('OR')
-                            </span>
-                        </div>
-                    @endif
+                    <div class="d-flex justify-content-center gap-3 mb-4">
+                        @if ($socialLoginActive)
+                            <div class="d-flex justify-content-center mb-2">
+                                @include('Template::partials.social_login')
+                            </div>
+                        @endif
+                    </div>
 
                     <form class="verify-gcaptcha" action="{{ route('user.register') }}" method="POST">
                         @csrf
@@ -459,9 +452,9 @@
                                         <i class="fas fa-user" style="font-size: 14px;"></i>
                                     </span>
                                     <input type="text" name="firstname"
-                                        class="form-control border-0 px-3 shadow-none"
-                                        placeholder="@lang('First name')" value="{{ old('firstname') }}"
-                                        style="font-size: 14px; padding: 12px 0;" required>
+                                        class="form-control border-0 px-3 shadow-none" placeholder="@lang('First name')"
+                                        value="{{ old('firstname') }}" style="font-size: 14px; padding: 12px 0;"
+                                        required>
                                 </div>
                             </div>
 
