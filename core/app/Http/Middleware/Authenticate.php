f0bf9afff34e3cc66e6412c9ef75be10f75d2301
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-17 23:28:16 -0500

    update logout auth massage

--- core/app/Http/Middleware/Authenticate.php
+++ core/app/Http/Middleware/Authenticate.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('user.login');
+    * Get the path the user should be redirected to when they are not authenticated.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return string|null
+    */
+    protected function redirectTo( $request ) {
+        if ( ! $request->expectsJson() ) {
+            return url( '/' );
         }
+    }
+
+    /**
+    * Handle an unauthenticated user.
+    * * এখানে আমরা ইউজার লগইন না থাকলে নোটিফিকেশন সেশন সেট করে দেব।
+    */
+    protected function unauthenticated( $request, array $guards ) {
+        if ( ! $request->expectsJson() ) {
+            $notify[] = [ 'error', 'Please login to access this page.' ];
+            session()->flash( 'notify', $notify );
+        }
+
+        parent::unauthenticated( $request, $guards );
     }
 }
