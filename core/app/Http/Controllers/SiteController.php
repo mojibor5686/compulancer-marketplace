78f739064f76c0d0bc7bab288460008826f74d0d
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-20 12:45:05 -0500

    update eroror

--- core/app/Http/Controllers/SiteController.php
+++ core/app/Http/Controllers/SiteController.php
@@ -20,70 +20,63 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Validator;
 
-
-class SiteController extends Controller
-{
-    public function index(Request $request)
-    {
-        if (isset($_GET['reference'])) {
-            $reference = $_GET['reference'];
-            session()->put('reference', $reference);
+class SiteController extends Controller {
+    public function index( Request $request ) {
+        if ( isset( $_GET[ 'reference' ] ) ) {
+            $reference = $_GET[ 'reference' ];
+            session()->put( 'reference', $reference );
         }
 
         $pageTitle = 'Home';
-        $type      = gs('default_service') ?? "service";
-        $featured  = "true";
+        $type      = gs( 'default_service' ) ?? 'service';
+        $featured  = 'true';
 
-        return view('Template::home', compact('pageTitle', 'featured', 'type'));
+        return view( 'Template::home', compact( 'pageTitle', 'featured', 'type' ) );
     }
 
-    public function pusher(Request $request)
-    {
+    public function pusher( Request $request ) {
         $general = gs();
 
         // Validate request parameters
-        $request->validate([
+        $request->validate( [
             'socket_id' => 'required|string',
             'channel_name' => 'required|string',
-        ]);
+        ] );
 
         $pusherSecret = $general->pusher_config->app_secret_key;
-        $socketId = $request->input('socket_id');
-        $channelName = $request->input('channel_name');
+        $socketId = $request->input( 'socket_id' );
+        $channelName = $request->input( 'channel_name' );
 
         // Create the authentication string
         $str = $socketId . ':' . $channelName;
-        $hash = hash_hmac('sha256', $str, $pusherSecret);
+        $hash = hash_hmac( 'sha256', $str, $pusherSecret );
 
-        return response()->json([
+        return response()->json( [
             'success' => true,
-            'message' => "Pusher authentication successful",
+            'message' => 'Pusher authentication successful',
             'auth'    => $general->pusher_config->app_key . ':' . $hash,
-        ]);
+        ] );
     }
 
-
-    public function contact()
-    {
-        $pageTitle = "Contact Us";
+    public function contact() {
+        $pageTitle = 'Contact Us';
         $user      = auth()->user();
-        return view('Template::contact', compact('pageTitle', 'user'));
+        return view( 'Template::contact', compact( 'pageTitle', 'user' ) );
     }
 
-    public function contactSubmit(Request $request)
-    {
-        $request->validate([
+    public function contactSubmit( Request $request ) {
+        $request->validate( [
             'name'    => 'required',
             'email'   => 'required',
             'subject' => 'required|string|max:255',
             'message' => 'required',
-        ]);
+        ] );
 
         $request->session()->regenerateToken();
 
-        if (!verifyCaptcha()) {
-            $notify[] = ['error', 'Invalid captcha provided'];
-            return back()->withNotify($notify);
+        if ( !verifyCaptcha() ) {
+            $notify[] = [ 'error', 'Invalid captcha provided' ];
+            return back()->withNotify( $notify );
         }
 
         $random = getNumber();
@@ -94,7 +87,6 @@ class SiteController extends Controller
         $ticket->email    = $request->email;
         $ticket->priority = Status::PRIORITY_MEDIUM;
 
-
         $ticket->ticket     = $random;
         $ticket->subject    = $request->subject;
         $ticket->last_reply = Carbon::now();
@@ -104,7 +96,7 @@ class SiteController extends Controller
         $adminNotification            = new AdminNotification();
         $adminNotification->user_id   = auth()->user() ? auth()->user()->id : 0;
         $adminNotification->title     = 'A new contact message has been submitted';
-        $adminNotification->click_url = urlPath('admin.ticket.view', $ticket->id);
+        $adminNotification->click_url = urlPath( 'admin.ticket.view', $ticket->id );
         $adminNotification->save();
 
         $message                    = new SupportMessage();
@@ -112,171 +104,164 @@ class SiteController extends Controller
         $message->message           = $request->message;
         $message->save();
 
-        $notify[] = ['success', 'Ticket created successfully!'];
+        $notify[] = [ 'success', 'Ticket created successfully!' ];
 
-        return to_route('ticket.view', [$ticket->ticket])->withNotify($notify);
+        return to_route( 'ticket.view', [ $ticket->ticket ] )->withNotify( $notify );
     }
 
-    public function policyPages($slug)
-    {
-        $policy = Frontend::where('tempname', activeTemplateName())->where('slug', $slug)->where('data_keys', 'policy_pages.element')->firstOrFail();
+    public function policyPages( $slug ) {
+        $policy = Frontend::where( 'tempname', activeTemplateName() )->where( 'slug', $slug )->where( 'data_keys', 'policy_pages.element' )->firstOrFail();
         $pageTitle   = $policy->data_values->title;
         $seoContents = $policy->seo_content;
-        $seoImage    = @$seoContents->image ? frontendImage('policy_pages', $seoContents->image, getFileSize('seo'), true) : null;
-        return view('Template::policy', compact('policy', 'pageTitle', 'seoContents', 'seoImage'));
+        $seoImage    = @$seoContents->image ? frontendImage( 'policy_pages', $seoContents->image, getFileSize( 'seo' ), true ) : null;
+        return view( 'Template::policy', compact( 'policy', 'pageTitle', 'seoContents', 'seoImage' ) );
     }
 
-    public function changeLanguage($lang = null)
-    {
-        $language          = Language::where('code', $lang)->first();
-        if (!$language) $lang = 'en';
-        session()->put('lang', $lang);
+    public function changeLanguage( $lang = null ) {
+        $language          = Language::where( 'code', $lang )->first();
+        if ( !$language ) $lang = 'en';
+        session()->put( 'lang', $lang );
         return back();
     }
 
-    public function blogs()
-    {
-        $blogs     = Frontend::where('data_keys', 'blog.element')->where('tempname', activeTemplateName())->paginate(getPaginate());
-        $pageTitle = "Blogs";
-        return view('Template::blogs', compact('blogs', 'pageTitle'));
+    public function blogs() {
+        $blogs     = Frontend::where( 'data_keys', 'blog.element' )->where( 'tempname', activeTemplateName() )->paginate( getPaginate() );
+        $pageTitle = 'Blogs';
+        return view( 'Template::blogs', compact( 'blogs', 'pageTitle' ) );
     }
 
+    public function blogDetails( $slug ) {
+        $blog        = Frontend::where( 'slug', $slug )->where( 'data_keys', 'blog.element' )->firstOrFail();
 
-    public function blogDetails($slug)
-    {
-        $blog        = Frontend::where('slug', $slug)->where('data_keys', 'blog.element')->firstOrFail();
-
-        // Fetch the latest blogs (you can adjust the number of blogs you want to fetch, e.g., latest 5)
-        $latestBlogs = Frontend::where('data_keys', 'blog.element')
-            ->where('slug', '!=', $slug)
-            ->latest()
-            ->limit(5)
-            ->get();
+        // Fetch the latest blogs ( you can adjust the number of blogs you want to fetch, e.g., latest 5 )
+        $latestBlogs = Frontend::where( 'data_keys', 'blog.element' )
+        ->where( 'slug', '!=', $slug )
+        ->latest()
+        ->limit( 5 )
+        ->get();
 
         $pageTitle   = 'Blog Details';
         $seoContents = $blog->seo_content;
-        $seoImage    = @$seoContents->image ? frontendImage('blog', $seoContents->image, getFileSize('seo'), true) : null;
+        $seoImage    = @$seoContents->image ? frontendImage( 'blog', $seoContents->image, getFileSize( 'seo' ), true ) : null;
 
-        return view('Template::blog_details', compact('blog', 'pageTitle', 'seoContents', 'seoImage', 'latestBlogs'));
+        return view( 'Template::blog_details', compact( 'blog', 'pageTitle', 'seoContents', 'seoImage', 'latestBlogs' ) );
     }
 
-
-    public function cookieAccept()
-    {
-        Cookie::queue('gdpr_cookie', gs('site_name'), 43200);
+    public function cookieAccept() {
+        Cookie::queue( 'gdpr_cookie', gs( 'site_name' ), 43200 );
     }
 
-    public function cookiePolicy()
-    {
-        $cookieContent = Frontend::where('data_keys', 'cookie.data')->first();
-        abort_if($cookieContent->data_values->status != Status::ENABLE, 404);
+    public function cookiePolicy() {
+        $cookieContent = Frontend::where( 'data_keys', 'cookie.data' )->first();
+        abort_if ( $cookieContent->data_values->status != Status::ENABLE, 404 );
         $pageTitle = 'Cookie Policy';
-        $cookie    = Frontend::where('data_keys', 'cookie.data')->first();
-        return view('Template::cookie', compact('pageTitle', 'cookie'));
+        $cookie    = Frontend::where( 'data_keys', 'cookie.data' )->first();
+        return view( 'Template::cookie', compact( 'pageTitle', 'cookie' ) );
     }
 
-    public function placeholderImage($size = null)
-    {
-        $imgWidth  = explode('x', $size)[0];
-        $imgHeight = explode('x', $size)[1];
+    public function placeholderImage( $size = null ) {
+        $imgWidth  = explode( 'x', $size )[ 0 ];
+        $imgHeight = explode( 'x', $size )[ 1 ];
         $text      = $imgWidth . '×' . $imgHeight;
-        $fontFile  = realpath('assets/font/solaimanLipi_bold.ttf');
-        $fontSize  = round(($imgWidth - 50) / 8);
-        if ($fontSize <= 9) {
+        $fontFile  = realpath( 'assets/font/solaimanLipi_bold.ttf' );
+        $fontSize  = round( ( $imgWidth - 50 ) / 8 );
+        if ( $fontSize <= 9 ) {
             $fontSize = 9;
         }
-        if ($imgHeight < 100 && $fontSize > 30) {
+        if ( $imgHeight < 100 && $fontSize > 30 ) {
             $fontSize = 30;
         }
 
-        $image     = imagecreatetruecolor($imgWidth, $imgHeight);
-        $colorFill = imagecolorallocate($image, 100, 100, 100);
-        $bgFill    = imagecolorallocate($image, 255, 255, 255);
-        imagefill($image, 0, 0, $bgFill);
-        $textBox    = imagettfbbox($fontSize, 0, $fontFile, $text);
-        $textWidth  = abs($textBox[4] - $textBox[0]);
-        $textHeight = abs($textBox[5] - $textBox[1]);
-        $textX      = ($imgWidth - $textWidth) / 2;
-        $textY      = ($imgHeight + $textHeight) / 2;
-        header('Content-Type: image/jpeg');
-        imagettftext($image, $fontSize, 0, $textX, $textY, $colorFill, $fontFile, $text);
-        imagejpeg($image);
-        imagedestroy($image);
+        $image     = imagecreatetruecolor( $imgWidth, $imgHeight );
+        $colorFill = imagecolorallocate( $image, 100, 100, 100 );
+        $bgFill    = imagecolorallocate( $image, 255, 255, 255 );
+        imagefill( $image, 0, 0, $bgFill );
+        $textBox    = imagettfbbox( $fontSize, 0, $fontFile, $text );
+        $textWidth  = abs( $textBox[ 4 ] - $textBox[ 0 ] );
+        $textHeight = abs( $textBox[ 5 ] - $textBox[ 1 ] );
+        $textX      = ( $imgWidth - $textWidth ) / 2;
+        $textY      = ( $imgHeight + $textHeight ) / 2;
+        header( 'Content-Type: image/jpeg' );
+        imagettftext( $image, $fontSize, 0, $textX, $textY, $colorFill, $fontFile, $text );
+        imagejpeg( $image );
+        imagedestroy( $image );
     }
 
-    public function maintenance()
-    {
+    public function maintenance() {
         $pageTitle = 'Maintenance Mode';
-        if (gs('maintenance_mode') == Status::DISABLE) {
-            return to_route('home');
+        if ( gs( 'maintenance_mode' ) == Status::DISABLE ) {
+            return to_route( 'home' );
         }
-        $maintenance = Frontend::where('data_keys', 'maintenance.data')->first();
-        return view('Template::maintenance', compact('pageTitle', 'maintenance'));
+        $maintenance = Frontend::where( 'data_keys', 'maintenance.data' )->first();
+        return view( 'Template::maintenance', compact( 'pageTitle', 'maintenance' ) );
     }
 
-    public function fileDownload($fileName, $type)
-    {
+    public function fileDownload( $fileName, $type ) {
         try {
-            $fileName = decrypt($fileName);
-        } catch (Exception $ex) {
-            $notify[] = ['error', "Invalid URL."];
-            return back()->withNotify($notify);
-        }
-        if ($type == 'file') {
-            $file     = Software::where('software_file', $fileName)->firstOrFail();
-            $filePath = getFilePath('softwareFile') . '/' . $file->software_file;
-        } elseif ($type == 'documentation') {
-            $file     = Software::where('document_file', $fileName)->firstOrFail();
-            $filePath = getFilePath('documentFile') . '/' . $file->document_file;
-        } elseif ($type == 'workFile') {
-            $file     = WorkFile::where('file', $fileName)->firstOrFail();
-            $filePath = getFilePath('workFile') . '/' . $file->file;
-        } elseif ($type == 'chatFile') {
-            $file     = Chat::where('file', $fileName)->firstOrFail();
-            $filePath = getFilePath('chatFile') . '/' . $file->file;
-        } elseif ($type == 'messageFile') {
-            $file     = Message::where('file', $fileName)->firstOrFail();
-            $filePath = getFilePath('messageFile') . '/' . $file->file;
-        } else {
-            $notify[] = ['error', 'Invalid file download request'];
-            return back()->withNotify($notify);
+            $fileName = decrypt( $fileName );
+
+            if ( empty( $fileName ) ) {
+                $notify[] = [ 'error', 'Invalid file name.' ];
+                return back()->withNotify( $notify );
+            }
+        } catch ( Exception $ex ) {
+            $notify[] = [ 'error', 'Invalid URL.' ];
+            return back()->withNotify( $notify );
         }
 
-        if (!file_exists($filePath)) {
-            $notify[] = ['error', "File dose not exists"];
-            return back()->withNotify($notify);
+        if ( $type == 'file' ) {
+            $file     = Software::where( 'software_file', $fileName )->firstOrFail();
+            $filePath = getFilePath( 'softwareFile' ) . '/' . $file->software_file;
+        } elseif ( $type == 'documentation' ) {
+            $file     = Software::where( 'document_file', $fileName )->firstOrFail();
+            $filePath = getFilePath( 'documentFile' ) . '/' . $file->document_file;
+        } elseif ( $type == 'workFile' ) {
+            $file     = WorkFile::where( 'file', $fileName )->firstOrFail();
+            $filePath = getFilePath( 'workFile' ) . '/' . $file->file;
+        } elseif ( $type == 'chatFile' ) {
+            $file     = Chat::where( 'file', $fileName )->firstOrFail();
+            $filePath = getFilePath( 'chatFile' ) . '/' . $file->file;
+        } elseif ( $type == 'messageFile' ) {
+            $file     = Message::where( 'file', $fileName )->firstOrFail();
+            $filePath = getFilePath( 'messageFile' ) . '/' . $file->file;
+        } else {
+            $notify[] = [ 'error', 'Invalid file download request' ];
+            return back()->withNotify( $notify );
         }
-        return response()->download($filePath);
+
+        if ( !file_exists( $filePath ) || is_dir( $filePath ) ) {
+            $notify[] = [ 'error', 'File does not exist' ];
+            return back()->withNotify( $notify );
+        }
+
+        return response()->download( $filePath );
     }
 
-    public function adRedirect($id)
-    {
-        $id         = decrypt($id);
-        $ad         = Advertisement::findOrFail($id);
+    public function adRedirect( $id ) {
+        $id         = decrypt( $id );
+        $ad         = Advertisement::findOrFail( $id );
         $ad->click += 1;
         $ad->save();
 
-        if ($ad->type == 'image') {
-            return redirect($ad->redirect_url);
+        if ( $ad->type == 'image' ) {
+            return redirect( $ad->redirect_url );
         }
         return back();
     }
 
-
-    public function subscriberStore(Request $request)
-    {
-        $validate = Validator::make($request->all(), [
+    public function subscriberStore( Request $request ) {
+        $validate = Validator::make( $request->all(), [
             'email' => 'required|email|unique:subscribers',
-        ]);
+        ] );
 
-        if ($validate->fails()) {
-            return response()->json(['error' => $validate->errors()->all()]);
+        if ( $validate->fails() ) {
+            return response()->json( [ 'error' => $validate->errors()->all() ] );
         }
 
         $subscriber        = new Subscriber();
         $subscriber->email = $request->email;
         $subscriber->save();
 
-        return response()->json(['success' => 'Subscribed successfully!']);
+        return response()->json( [ 'success' => 'Subscribed successfully!' ] );
     }
 }
