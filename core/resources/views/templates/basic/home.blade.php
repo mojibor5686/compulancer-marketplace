d1a8d2a6a5975c3374d1638e0f0ed16bd86da764
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-22 13:15:26 -0500

    update

--- core/resources/views/templates/basic/home.blade.php
+++ core/resources/views/templates/basic/home.blade.php
@@ -107,10 +107,6 @@
                             </div>
 
                         </div>
-
-                        <div class="col-lg-4 col-xl-3">
-                            @include('Template::partials.filter')
-                        </div>
                     </div>
 
                 </div>
