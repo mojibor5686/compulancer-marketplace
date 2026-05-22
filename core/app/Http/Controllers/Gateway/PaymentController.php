dc4119fd503d61d0e512ebe38c34b8322e56593f
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-22 09:46:56 -0500

    debug

--- core/app/Http/Controllers/Gateway/PaymentController.php
+++ core/app/Http/Controllers/Gateway/PaymentController.php
@@ -71,6 +71,8 @@ public function depositInsert( Request $request,  $orderNumber = null ) {
             return redirect($successUrl);
         }
 
+        dd($request->all(), $successUrl, $failUrl);
+
         // ==========================================
         // ২. ডাইনামিক UddoktaPay গেটওয়ে প্রসেস
         // ==========================================
