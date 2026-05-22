1b9a7dfd8e00956191df7f28263451b89957f19a
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-21 23:45:48 -0500

    updated

--- core/resources/views/templates/basic/service/service_details.blade.php
+++ core/resources/views/templates/basic/service/service_details.blade.php
@@ -127,9 +127,8 @@
                                         </button>
                                     </li>
                                     <li class="nav-item" role="presentation">
-                                        <button class="nav-link comments-tab-btn" data-bs-dismiss="modal"
-                                            data-bs-toggle="modal" data-bs-target="#signInModal" type="button"
-                                            role="tab">
+                                        <button class="nav-link comments-tab-btn" data-bs-toggle="tab"
+                                            data-bs-target="#jss-details-tab-4" type="button" role="tab">
                                             @lang('Comments')
                                         </button>
                                     </li>
@@ -568,7 +567,7 @@
                 @guest
                 $('.comments-tab-btn').on('click', function(e) {
                     e.preventDefault(); // Prevent default tab behavior
-                    $('#signInModal').modal('show'); // Show the login modal
+                    $('#loginModal').modal('show'); // Show the login modal
                 });
             @endguest
 
