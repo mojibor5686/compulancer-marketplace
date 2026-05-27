293e7469a0ad15d60c57ba06abfb5ba72e3b75c2
Md Mojibor Rahman <mojibor6321@gmail.com>
2026-05-27 11:15:11 -0500

    updated

--- core/resources/views/templates/basic/home.blade.php
+++ core/resources/views/templates/basic/home.blade.php
@@ -66,7 +66,8 @@
                                         $totalReviews = $product->total_review > 0 ? $product->total_review : 0;
                                     @endphp
 
-                                    <div class="col productListCol kwrk-item-{{ $loop->iteration }}">
+                                    <div class="col productListCol kwrk-item-{{ $loop->iteration }}"
+                                        style="margin-top: 20px !important;">
                                         <article class="card jss--card jss--card-{{ $type }}"
                                             style="background: #ffffff !important; border: 1px solid #dadbdd !important; border-radius: 4px !important; box-shadow: none !important; overflow: hidden !important; display: flex !important; flex-direction: column !important; height: 100% !important; font-family: Macan, 'Helvetica Neue', Helvetica, Arial, sans-serif !important; position: relative !important; transition: all 0.2s ease-in-out;">
 
